3898add070c67361f880ec9ece7f0600db7f5e95
eunseok <tkdlqfpdlej@naver.com>
2024-10-09 17:48:34 +0900

    feat/ 목표 페이지 - 활동 대사량 추가

--- client/src/component/BmrCalculator.jsx
+++ client/src/component/BmrCalculator.jsx
@@ -2,9 +2,11 @@ import React, {useState} from "react";
 
 function BmrCalculator( {formData} ) {
     const [result, setResult] = useState("")
+    const [result2,setResult2] = useState("")
     
     const calculateBmr = () => {
-        const {weight, height, age, gender} = formData;    
+        const {weight, height, age,
+              gender, activityGrade, exerciseGrade} = formData;    
     
         if (!weight || !height || !age || !gender) {
             setResult("값을 다 입력해주세요.");
@@ -14,22 +16,28 @@ function BmrCalculator( {formData} ) {
         const w = parseFloat(weight);
         const h = parseFloat(height);
         const a = parseInt(age);
-
+        const aG = parseFloat(activityGrade);
+        const eG = parseFloat(exerciseGrade);
         let bmr;
+        let amr;
 
         if (gender === "male") {
           bmr = 88.362 + (13.397 * w) + (4.799 * h) - (5.667 * a);
+          amr = (88.362 + (13.397 * w) + (4.799 * h) - (5.667 * a))*(aG+eG);
         } else {
           bmr = 447.593 + (9.247 * w) + (3.098 * h) - (4.330 * a);
+          amr = (447.593 + (9.247 * w) + (3.098 * h) - (4.330 * a))*(aG+eG);
         }
 
         setResult(`기초대사량 : ${Math.round(bmr)} kcal`) 
+        setResult2(`활동대사량 : ${Math.round(amr)} kcal`)
     }; return (
         <div>
     <button onClick={calculateBmr}>계산하기</button>
-    {result && (
+    {result && result2 && (
         <div>
           <p>{result}</p>
+          <p>{result2}</p>
         </div>
       )}
     </div>
