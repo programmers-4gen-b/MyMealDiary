314b2405c80d958d3e9dcfffaf586cebfbc8a63c
robin <nowrobin3@gmail.com>
2024-10-09 17:07:16 +0900

    feat/ supabase 연결

--- server/app.js
+++ server/app.js
@@ -11,3 +11,17 @@ app.listen(process.env.PORT, () => {
 const findFoodRouter = require('./route/processedFood/findFood');
 
 app.use('/findFood', findFoodRouter);
+
+const { createClient } = require('@supabase/supabase-js');
+const supabaseUrl = process.env.SUPABASE_URL;  // .env 파일에서 가져옴
+const supabaseKey = process.env.SUPABASE_KEY;  // .env 파일에서 가져옴
+const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+
+app.get('/test', async (req, res) => {
+   const { data:users, error: err1 } = await supabase.from('users').select('*');
+   const { data:meal_log, error: err2 } = await supabase.from('meal_log').select('*');
+   if (err1) {
+      return res.status(500).json({ error: error.message });
+   }
+   res.json({users, meal_log});
+});
