547e2124c3da09cb98dc1f8e5a5aedf2f99cd0b2
vedivero <vedivero@github.com>
2024-10-10 18:55:59 +0900

    feat/processedFood - 사용자 섭취 가공식품 DB입력 구현(테스트)

--- server/route/processedFood/findFood.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { findFood } = require('../../controller/processdFood/FindFoodController');
-
-router.get('/', findFood);
-
-module.exports = router;
