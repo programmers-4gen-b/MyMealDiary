875351c46185fb5885197004a94486c4c456affa
vedivero <vedivero@github.com>
2024-10-11 11:27:54 +0900

    feat/processedFood - 기존 OPEN API 식품 조회 로직을, 데이터베이스 조회 방식으로 변경

--- server/controller/processdFood/ProcessedFoodController.js
+++ server/controller/processdFood/ProcessedFoodController.js
@@ -9,78 +9,17 @@ const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY
 
 const findProcessedFood = async (req, res) => {
    try {
-      let foodNm = req.query.foodNm;
-      const apiUrl = `http://api.data.go.kr/openapi/tn_pubr_public_nutri_process_info_api?serviceKey=${
-         process.env.SERVICE_KEY_PROCESSED_FOOD
-      }&pageNo=1&numOfRows=100&type=json&foodNm=${encodeURIComponent(foodNm)}`;
+      let foodnm = req.query.foodNm;
+      const { data: processedfood, error } = await supabase
+         .from('processedfood')
+         .select('*')
+         .like('foodnm', `%${foodnm}%`); // foodnm에 포함된 문자열 검색
 
-      const response = await axios.get(apiUrl);
+      if (error) return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: error.message });
 
-      let foodData = response.data.response.body.items;
-
-      if (!foodData || foodData.length === 0) {
-         return res.status(StatusCodes.NOT_FOUND).json({
-            message: '해당 음식에 대한 데이터를 찾을 수 없습니다.',
-         });
-      }
-
-      const fieldsToExclude = [
-         'foodOriginCd',
-         'dataCd',
-         'nat',
-         'vitaRae',
-         'retol',
-         'cartb',
-         'thia',
-         'ribf',
-         'nia',
-         'vitc',
-         'vitd',
-         'chole',
-         'fasat',
-         'fatrn',
-         'srcCd',
-         'srcNm',
-         'servSize',
-         'foodSize',
-         'itemMnftrRptNo',
-         'mfrNm',
-         'imptNm',
-         'distNm',
-         'imptYn',
-         'cooCd',
-         'cooNm',
-         'dataProdCd',
-         'dataProdNm',
-         'crtYmd',
-         'crtrYmd',
-         'insttCode',
-         'foodLv3Cd',
-         'foodLv3Nm',
-         'foodLv4Cd',
-         'foodLv4Nm',
-         'foodLv5Cd',
-         'foodLv5Nm',
-         'foodLv6Cd',
-         'foodLv6Nm',
-         'foodLv7Cd',
-         'foodLv7Nm',
-      ];
-
-      foodData = foodData.map((item) => {
-         const filteredItem = Object.fromEntries(
-            Object.entries(item).filter(([key]) => !fieldsToExclude.includes(key)),
-         );
-         return filteredItem;
-      });
-
-      return res.status(StatusCodes.OK).json(foodData);
+      res.status(StatusCodes.OK).json({ processedfood });
    } catch (error) {
-      console.error(error);
-      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-         message: '서버 에러가 발생했습니다.',
-         error: error.message,
-      });
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ error: '가공식품 조회 에러 ', error });
    }
 };
 
