bafbbb1f7581a1162af8ea4ae1d782b259c5d0df
DESKTOP-P2N46VN\hyeon <kkul2000@naver.com>
2024-10-11 14:20:31 +0900

    feat/ 음식 추가 페이지 - api 연동, 검색버튼 추가

--- client/src/pages/AddFood.js
+++ client/src/pages/AddFood.js
@@ -1,18 +1,8 @@
-import "../css/common.css"
+import "../css/common.css";
 import React from "react";
 import { useState, useEffect } from "react";
 import _ from "lodash";
 import FoodModal from "./FoodModal.js";
-
-//테스트용 임시 더미리스트
-const dummyFoodList = [
-  { foodCd: 1, foodNm: "사과" },
-  { foodCd: 2, foodNm: "바나나" },
-  { foodCd: 3, foodNm: "오렌지" },
-  { foodCd: 4, foodNm: "오렌지주스" },
-  { foodCd: 5, foodNm: "포도주스" },
-  { foodCd: 6, foodNm: "망고" },
-];
 
 function AddFood() {
   const [query, setQuery] = useState("");
@@ -20,31 +10,16 @@ function AddFood() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [selectedFood, setSelectedFood] = useState(null);
 
-  //디바운싱하여 api 호출
-  const debouncedSearch = _.debounce((searchTerm) => {
-    fetch(`http://localhost:4545/findFood?foodNm=${searchTerm}`)
-      .then((response) => response.json())
-      .then((data) => setResults(data))
-      .catch((err) => console.error(err));
-  }, 300);
-
-  useEffect(() => {
+  const handleSearch = () => {
     if (query) {
-      debouncedSearch(query);
+      fetch(`http://localhost:4545/processedFood?foodNm=${query}`)
+        .then((response) => response.json())
+        .then((data) => setResults(data))
+        .catch((err) => console.error(err));
+    } else {
+      setResults([]);
     }
-  }, [query]);
-
-  //더미리스트 사용 임시코드
-  // useEffect(() => {
-  //   if (query) {
-  //     const filteredResults = dummyFoodList.filter((food) =>
-  //       food.foodNm.includes(query)
-  //     );
-  //     setResults(filteredResults);
-  //   } else {
-  //     setResults([]);
-  //   }
-  // }, [query]);
+  };
 
   const handleItemClick = (food) => {
     setSelectedFood(food);
@@ -56,8 +31,6 @@ function AddFood() {
     setSelectedFood(null);
   };
 
-  console.log(results);
-
   return (
     <div className="app-container">
       <input
@@ -66,13 +39,18 @@ function AddFood() {
         onChange={(e) => setQuery(e.target.value)}
         placeholder="음식을 입력하세요"
       />
-      {/* <ul>
-        {results.map((food) => (
-          <li key={food.foodCd} onClick={() => handleItemClick(food)}>
-            {food.foodNm}
-          </li>
-        ))}
-      </ul> */}
+      <button onClick={handleSearch}>검색</button>
+      <ul>
+        {results.processedfood && results.processedfood.length > 0 && (
+          <ul>
+            {results.processedfood.slice(0, 20).map((food) => (
+              <li key={food.foodcd} onClick={() => handleItemClick(food)}>
+                {food.foodnm}
+              </li>
+            ))}
+          </ul>
+        )}
+      </ul>
 
       <FoodModal
         isOpen={isModalOpen}
